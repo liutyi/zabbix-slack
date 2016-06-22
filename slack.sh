0f9b9ae14a27f5987bdf594eb1d512b324c3668b
Oleksandr <liutyi@users.noreply.github.com>
2016-06-22 19:39:44 +0300

    switch from sed to tr in param parser

--- slack.sh
+++ slack.sh
@@ -24,15 +24,15 @@ chart_basedir="/tmp/slack_charts"
 chart_cookie="/tmp/zcookies.txt"
 
 # set params
-host="`echo \"${params}\" | grep 'HOST: ' | awk -F'HOST: ' '{print $2}' | sed -e 's///g'`"
-trigger_name="`echo \"${params}\" | grep 'TRIGGER_NAME: ' | awk -F'TRIGGER_NAME: ' '{print $2}' | sed -e 's///g'`"
-trigger_status="`echo \"${params}\" | grep 'TRIGGER_STATUS: ' | awk -F'TRIGGER_STATUS: ' '{print $2}' | sed -e 's///g'`"
-trigger_severity="`echo \"${params}\" | grep 'TRIGGER_SEVERITY: ' | awk -F'TRIGGER_SEVERITY: ' '{print $2}' | sed -e 's///g'`"
-trigger_url="`echo \"${params}\" | grep 'TRIGGER_URL: ' | awk -F'TRIGGER_URL: ' '{print $2}' | sed -e 's///g'`"
-datetime="`echo \"${params}\" | grep 'DATETIME: ' | awk -F'DATETIME: ' '{print $2}' | sed -e 's///g'`"
-item_value="`echo \"${params}\" | grep 'ITEM_VALUE: ' | awk -F'ITEM_VALUE: ' '{print $2}' | sed -e 's///g'`"
-event_id="`echo \"${params}\" | grep 'EVENT_ID: ' | awk -F'EVENT_ID: ' '{print $2}' | sed -e 's///g'`"
-item_id="`echo \"${params}\" | grep 'ITEM_ID: ' | awk -F'ITEM_ID: ' '{print $2}' | sed -e 's///g'`"
+host="`echo \"${params}\" | grep 'HOST: ' | awk -F'HOST: ' '{print $2}' | tr --delete '\r\n'`"
+trigger_name="`echo \"${params}\" | grep 'TRIGGER_NAME: ' | awk -F'TRIGGER_NAME: ' '{print $2}' | tr --delete '\r\n'`"
+trigger_status="`echo \"${params}\" | grep 'TRIGGER_STATUS: ' | awk -F'TRIGGER_STATUS: ' '{print $2}' | tr --delete '\r\n'`"
+trigger_severity="`echo \"${params}\" | grep 'TRIGGER_SEVERITY: ' | awk -F'TRIGGER_SEVERITY: ' '{print $2}' | tr --delete '\r\n'`"
+trigger_url="`echo \"${params}\" | grep 'TRIGGER_URL: ' | awk -F'TRIGGER_URL: ' '{print $2}' | tr --delete '\r\n'`"
+datetime="`echo \"${params}\" | grep 'DATETIME: ' | awk -F'DATETIME: ' '{print $2}' | tr --delete '\r\n'`"
+item_value="`echo \"${params}\" | grep 'ITEM_VALUE: ' | awk -F'ITEM_VALUE: ' '{print $2}' |tr --delete '\r\n'`"
+event_id="`echo \"${params}\" | grep 'EVENT_ID: ' | awk -F'EVENT_ID: ' '{print $2}' | tr --delete '\r\n'`"
+item_id="`echo \"${params}\" | grep 'ITEM_ID: ' | awk -F'ITEM_ID: ' '{print $2}' | tr --delete '\r\n'`"
 
 # get charts
 if [ "${item_id}" != "" ]; then
@@ -45,7 +45,7 @@ if [ "${item_id}" != "" ]; then
 
     # if triger url is empty then we link to the graph with the item_id
     if [ "${trigger_url}" == "" ]; then
-        trigger_url="${zabbix_baseurl}/history.php?action=showgraph&itemids=${item_id}"
+        trigger_url="${zabbix_baseurl}/history.php?action=showgraph&itemids%5B%5D=${item_id}"
     fi
 fi
 
